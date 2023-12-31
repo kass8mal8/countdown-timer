587c543ec749bb74679afdd2db05ecdf10b057dd
Kassim Ali <105120332+kass8mal8@users.noreply.github.com>
2023-12-31 16:53:55 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return ( 
         <div className="footer">
-            Coded by <a href="https://github.com/kass8mal8">kass8mal8</a>
+            Coded with ❤️ by <a href="https://github.com/kass8mal8">kass8mal8</a>
         </div>
      );
 }
